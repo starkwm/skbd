3ba82147d95e35422e04c1304bebb76648e90e27
Tom Bell <tomb@tomb.io>
2022-04-17 18:48:50 +0100

    Add Lexer class

---
+++ Sources/skbdlib/Config/Lexer.swift
@@ -0,0 +1,105 @@
+import Alicia
+
+class Lexer {
+    private var buffer = ""
+    private var current = Character("\0")
+    private var readPos = 0
+
+    init(_ buffer: String) {
+        self.buffer = buffer
+
+        advance()
+    }
+
+    func getToken() -> Token {
+        skipWhitespace()
+
+        var token: Token
+
+        switch current {
+        case "\0":
+            token = Token(type: .endOfStream)
+        case "+":
+            token = Token(type: .plus)
+        case "-":
+            token = Token(type: .dash)
+        case "#":
+            skipComment()
+            token = Token(type: .comment)
+        case ":":
+            advance()
+            skipWhitespace()
+            let cmd = readCommand()
+            token = Token(type: .command, text: cmd)
+        default:
+            if current.isLetter {
+                let text = readIdentifier()
+                let type = resolveIdentifierType(identifier: text)
+                return Token(type: type, text: text)
+            } else {
+                token = Token(type: .unknown, text: String(current))
+            }
+        }
+
+        advance()
+
+        return token
+    }
+
+    private func advance() {
+        current = readPos >= buffer.count ? Character("\0") : buffer[readPos]
+        readPos += 1
+    }
+
+    private func skipWhitespace() {
+        while current.isWhitespace {
+            advance()
+        }
+    }
+
+    private func skipComment() {
+        while !current.isNewline, current != "\0" {
+            advance()
+        }
+    }
+
+    private func readCommand() -> String {
+        let start = readPos - 1
+
+        while !current.isNewline, current != "\0" {
+            if current == "\\" {
+                advance()
+            }
+
+            advance()
+        }
+
+        return buffer[start ..< readPos - 1]
+    }
+
+    private func readIdentifier() -> String {
+        let start = readPos - 1
+
+        while current.isLetter || current == "_" {
+            advance()
+        }
+
+        while current.isNumber {
+            advance()
+        }
+
+        return buffer[start ..< readPos - 1]
+    }
+
+    private func resolveIdentifierType(identifier: String) -> TokenType {
+        if identifier.count == 1 || keyToCode.keys.contains(identifier) {
+            return .key
+        }
+
+        if modifierIdentifiers.contains(identifier) {
+            return .modifier
+        }
+
+        return .unknown
+    }
+}
---
+++ Tests/skbdlibTests/Config/LexerTests.swift
@@ -0,0 +1,67 @@
+import XCTest
+
+@testable import skbdlib
+
+final class LexerTests: XCTestCase {
+    // MARK: Lexer#getToken
+
+    func testGetToken() {
+        let input = """
+            # this if the first comment
+            opt-space: open -a iTerm2.app
+
+            # this is a comment
+            # with multiple lines... + - ++
+            cmd + shift - a : echo "Hello world"
+
+            # this is another comment, followed by a multiline command
+            ctrl + opt - return : echo "foo bar"; \\
+                rm -fr /
+
+           # this is a comment with a hash in it # here
+           hyper - f1:
+               echo "# hello world"
+        """
+
+        let expected: [TokenType] = [
+            .comment,
+            .modifier, .dash, .key, .command,
+            .comment,
+            .comment,
+            .modifier, .plus, .modifier, .dash, .key, .command,
+            .comment,
+            .modifier, .plus, .modifier, .dash, .key, .command,
+            .comment,
+            .modifier, .dash, .key,
+            .command,
+            .endOfStream,
+        ]
+
+        let lexer = Lexer(input)
+
+        for type in expected {
+            let token = lexer.getToken()
+            XCTAssertEqual(token.type, type)
+        }
+    }
+
+    func testGetTokenWithUnknownToken() {
+        let lexer = Lexer(";")
+        let token = lexer.getToken()
+
+        XCTAssertEqual(token.type, .unknown)
+    }
+
+    func testGetTokenWithUnknownIdentifier() {
+        let lexer = Lexer("cmd - f100: ls")
+
+        let expected: [TokenType] = [
+            .modifier, .dash, .unknown, .command,
+        ]
+
+        for type in expected {
+            let token = lexer.getToken()
+            XCTAssertEqual(token.type, type)
+        }
+    }
+}
