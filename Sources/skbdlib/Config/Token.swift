ab0330b6a628de24e1b521324c2a6a9b74aaf750
Tom Bell <tomb@tomb.io>
2022-04-17 18:45:49 +0100

    Add Token struct

---
+++ Sources/skbdlib/Config/Token.swift
@@ -0,0 +1,8 @@
+struct Token {
+    var type: TokenType
+    var text: String?
+
+    var description: String {
+        "Token {type: \(type), text: \(text ?? "nil")}"
+    }
+}
---
+++ Tests/skbdlibTests/Config/TokenTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+@testable import skbdlib
+
+final class TokenTests: XCTestCase {
+    // MARK: Token#description
+
+    func testDescription() {
+        let token = Token(type: .plus)
+
+        XCTAssertEqual(token.description, "Token {type: plus, text: nil}")
+    }
+
+    func testDescriptionWithText() {
+        let token = Token(type: .command, text: "echo \"Hello World\"")
+
+        XCTAssertEqual(token.description, "Token {type: command, text: echo \"Hello World\"}")
+    }
+}
