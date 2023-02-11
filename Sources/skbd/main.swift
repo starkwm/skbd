84a292dc5eb845e3093865c6a2d00cecffdbb4f2
Tom Bell <tomb@tomb.io>
2023-02-11 01:03:25 +0000

    Add printing the version information to main

--- Sources/skbd/main.swift
+++ Sources/skbd/main.swift
@@ -1,1 +1,12 @@
-func main() {}
+import Foundation
+
+func main() -> Int32 {
+    if arguments.version {
+        fputs("skbd version \(Version.current.value)\n", stdout)
+        return EXIT_SUCCESS
+    }
+
+    return EXIT_SUCCESS
+}
+
+exit(main())
