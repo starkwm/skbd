90ead0d29481348cdccedd708ed93055474a8a67
Tom Bell <tomb@tomb.io>
2023-03-15 20:03:15 +0000

    Add option and flags to Arguments

--- Sources/skbd/Arguments.swift
+++ Sources/skbd/Arguments.swift
@@ -2,6 +2,12 @@ import ArgumentParser
 import Foundation
 
 struct Arguments: ParsableArguments {
+    @Option(name: .shortAndLong, help: ArgumentHelp("Path to the configuration file", valueName: "path"))
+    var config: String = ("~/.config/skbd/skbdrc" as NSString).resolvingSymlinksInPath
+
+    @Flag(name: .shortAndLong, help: "Reload the configuration file")
+    var reload = false
+
     @Flag(name: .shortAndLong, help: "Show version information")
     var version = false
 }
