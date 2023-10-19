e3681d524863f98c39a423ea5a87d30b51dc2fb0
Tom Bell <tomb@tomb.io>
2023-10-19 02:12:18 +0100

    Update minimum macOS target version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "skbd",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
