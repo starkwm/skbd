b493cb054bf9678d0494cb8b797bbe48edcc3fa2
Tom Bell <tomb@tomb.io>
2024-04-01 17:41:24 +0100

    Update tombell/alicia package to 3.0.0

--- Package.swift
+++ Package.swift
@@ -3,32 +3,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "skbd",
-    platforms: [
-        .macOS(.v14),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
-        .package(url: "https://github.com/starkwm/alicia", from: "2.0.4"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "skbd",
-            dependencies: [
-                "skbdlib",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ],
-            exclude: ["version.swift.tmpl"]
-        ),
-        .target(
-            name: "skbdlib",
-            dependencies: [
-                .product(name: "Alicia", package: "alicia"),
-            ]
-        ),
-        .testTarget(
-            name: "skbdlibTests",
-            dependencies: ["skbdlib"]
-        ),
-    ]
+  name: "skbd",
+  platforms: [
+    .macOS(.v14)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+    .package(url: "https://github.com/starkwm/alicia", from: "3.0.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "skbd",
+      dependencies: [
+        "skbdlib",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ],
+      exclude: ["version.swift.tmpl"]
+    ),
+    .target(
+      name: "skbdlib",
+      dependencies: [
+        .product(name: "Alicia", package: "alicia")
+      ]
+    ),
+    .testTarget(
+      name: "skbdlibTests",
+      dependencies: ["skbdlib"]
+    ),
+  ]
 )
