2d86270b65d6916ba0e209f3d9517c458059b7a7
Tom Bell <tomb@tomb.io>
2024-06-15 21:12:52 +0100

    Update version.swift and version.swift.tmpl formatting

--- Sources/skbd/version.swift
+++ Sources/skbd/version.swift
@@ -1,8 +1,8 @@
 /// A type for describing the version of skbd.
 struct Version {
-    /// The string value for the version.
-    let value: String
+  /// The string value for the version.
+  let value: String
 
-    /// The current version of skbd.
-    static let current = Self(value: "v0.0.3")
+  /// The current version of skbd.
+  static let current = Self(value: "v0.0.3")
 }
