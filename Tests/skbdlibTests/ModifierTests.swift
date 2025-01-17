9ab991de5fde6c1639744001f03bc13ad52e1a1a
Tom Bell <tomb@tombell.dev>
2025-01-17 23:44:21 +0000

    wip: modifier enum

--- Tests/skbdlibTests/ModifierTests.swift
+++ Tests/skbdlibTests/ModifierTests.swift
@@ -4,40 +4,50 @@ import XCTest
 @testable import skbdlib
 
 final class ModifierTests: XCTestCase {
+  // MARK: Modifier.valid
+
+  func testValid() {
+    XCTAssertTrue(Modifier.valid("shift"))
+  }
+
+  func testValidWithInvalidModifier() {
+    XCTAssertFalse(Modifier.valid("super-duper-modifier"))
+  }
+
   // MARK: Modifier.flags
 
-  func testModifierFlagsShift() {
+  func testFlagsWithShift() {
     XCTAssertEqual(Modifier.flags(for: ["shift"]), UInt32(shiftKey))
   }
 
-  func testModifierFlagsControl() {
+  func testFlagsWithControl() {
     XCTAssertEqual(Modifier.flags(for: ["ctrl"]), UInt32(controlKey))
     XCTAssertEqual(Modifier.flags(for: ["control"]), UInt32(controlKey))
   }
 
-  func testModifierFlagsOption() {
+  func testFlagsWithOption() {
     XCTAssertEqual(Modifier.flags(for: ["alt"]), UInt32(optionKey))
     XCTAssertEqual(Modifier.flags(for: ["opt"]), UInt32(optionKey))
     XCTAssertEqual(Modifier.flags(for: ["option"]), UInt32(optionKey))
   }
 
-  func testModifierFlagsCommand() {
+  func testFlagsWithCommand() {
     XCTAssertEqual(Modifier.flags(for: ["cmd"]), UInt32(cmdKey))
     XCTAssertEqual(Modifier.flags(for: ["command"]), UInt32(cmdKey))
   }
 
-  func testModifierFlagsHyper() {
+  func testFlagsWithHyper() {
     XCTAssertEqual(Modifier.flags(for: ["hyper"]), UInt32(cmdKey | optionKey | shiftKey | controlKey))
   }
 
-  func testModifierFlagsMultiple() {
+  func testFlagsWithMultiple() {
     XCTAssertEqual(
       Modifier.flags(for: ["shift", "ctrl", "alt", "cmd"]),
       UInt32(shiftKey | controlKey | optionKey | cmdKey)
     )
   }
 
-  func testModifierFlagsCaseInsensitivity() {
+  func testFlagsCaseInsensitivity() {
     XCTAssertEqual(
       Modifier.flags(for: ["shift", "CTRL", "AlT", "cMd"]),
       UInt32(shiftKey | controlKey | optionKey | cmdKey)
