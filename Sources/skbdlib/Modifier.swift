6e04caf368e23d4839a620f1ded627f78062d01c
Tom Bell <tomb@tombell.dev>
2025-01-17 23:37:09 +0000

    Add Modifier enum and tests

---
+++ Sources/skbdlib/Modifier.swift
@@ -0,0 +1,29 @@
+import Carbon
+
+private let modifiersToCode: [String: Int] = [
+  "shift": shiftKey,
+  "ctrl": controlKey, "control": controlKey,
+  "alt": optionKey, "opt": optionKey, "option": optionKey,
+  "cmd": cmdKey, "command": cmdKey,
+  "hyper": cmdKey | optionKey | shiftKey | controlKey,
+]
+
+enum Modifier {
+  static func valid(_ modifier: String) -> Bool {
+    modifiersToCode.keys.contains(modifier.lowercased())
+  }
+
+  static func flags(for modifiers: [String]) -> UInt32 {
+    let mods = Set(modifiers.map { $0.lowercased() })
+
+    var flags = 0
+
+    for mod in mods {
+      if let flag = modifiersToCode[mod] {
+        flags |= flag
+      }
+    }
+
+    return UInt32(flags)
+  }
+}
