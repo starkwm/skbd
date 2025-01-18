eb4b6aabd5ef19fe549850578f50bc42929b95cb
Tom Bell <tomb@tombell.dev>
2025-01-18 02:50:51 +0000

    Update Token to conform to Hashable

--- Sources/skbdlib/Token.swift
+++ Sources/skbdlib/Token.swift
@@ -1,8 +1,13 @@
-struct Token {
+struct Token: Hashable {
   var type: TokenType
   var text: String?
 
   var description: String {
     "Token {type: \(type), text: \(text ?? "nil")}"
+  }
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(type)
+    hasher.combine(text)
   }
 }
