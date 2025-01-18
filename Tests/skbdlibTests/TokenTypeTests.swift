c88d0ea5613a0a9c8817095017a39720c7908c4f
Tom Bell <tomb@tombell.dev>
2025-01-18 02:52:55 +0000

    Refactor TokenTypeTests to use Testing framework

--- Tests/skbdlibTests/TokenTypeTests.swift
+++ Tests/skbdlibTests/TokenTypeTests.swift
@@ -1,55 +1,26 @@
-import XCTest
+import Testing
 
 @testable import skbdlib
 
-class TokenTypeTests: XCTestCase {
+@Suite("TokenType")
+struct TokenTypeTests {
   // MARK: TokenType#description
 
-  func testTokenTypeComment() {
-    let tokenType: TokenType = .comment
-    XCTAssertEqual(tokenType, TokenType.comment)
-    XCTAssertEqual(tokenType.description, "TokenType {Comment}")
-  }
+  @Test("TokenType#description")
+  func description() async throws {
+    let tests: [TokenType: String] = [
+      .comment: "TokenType {Comment}",
+      .modifier: "TokenType {Modifier}",
+      .key: "TokenType {Key}",
+      .command: "TokenType {Command}",
+      .plus: "TokenType {+}",
+      .dash: "TokenType {-}",
+      .unknown: "TokenType {Unknown}",
+      .endOfStream: "TokenType {End of Stream}",
+    ]
 
-  func testTokenTypeModifier() {
-    let tokenType: TokenType = .modifier
-    XCTAssertEqual(tokenType, TokenType.modifier)
-    XCTAssertEqual(tokenType.description, "TokenType {Modifier}")
-  }
-
-  func testTokenTypeKey() {
-    let tokenType: TokenType = .key
-    XCTAssertEqual(tokenType, TokenType.key)
-    XCTAssertEqual(tokenType.description, "TokenType {Key}")
-  }
-
-  func testTokenTypeCommand() {
-    let tokenType: TokenType = .command
-    XCTAssertEqual(tokenType, TokenType.command)
-    XCTAssertEqual(tokenType.description, "TokenType {Command}")
-  }
-
-  func testTokenTypePlus() {
-    let tokenType: TokenType = .plus
-    XCTAssertEqual(tokenType, TokenType.plus)
-    XCTAssertEqual(tokenType.description, "TokenType {+}")
-  }
-
-  func testTokenTypeDash() {
-    let tokenType: TokenType = .dash
-    XCTAssertEqual(tokenType, TokenType.dash)
-    XCTAssertEqual(tokenType.description, "TokenType {-}")
-  }
-
-  func testTokenTypeUnknown() {
-    let tokenType: TokenType = .unknown
-    XCTAssertEqual(tokenType, TokenType.unknown)
-    XCTAssertEqual(tokenType.description, "TokenType {Unknown}")
-  }
-
-  func testTokenTypeEndOfStream() {
-    let tokenType: TokenType = .endOfStream
-    XCTAssertEqual(tokenType, TokenType.endOfStream)
-    XCTAssertEqual(tokenType.description, "TokenType {End of Stream}")
+    for (tokenType, expected) in tests {
+      #expect(tokenType.description == expected)
+    }
   }
 }
