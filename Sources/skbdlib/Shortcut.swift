9cea9d05eae2a7400bd1eba0053771688ca3dec5
Tom Bell <tomb@tombell.dev>
2025-01-19 01:37:11 +0000

    Update how handlers for Shortcut is setup

--- Sources/skbdlib/Shortcut.swift
+++ Sources/skbdlib/Shortcut.swift
@@ -2,6 +2,19 @@ import Carbon
 import Foundation
 
 public struct Shortcut {
+  public static func handler(for command: String) -> (() -> Void) {
+    let shell = ProcessInfo.processInfo.environment["SHELL"].flatMap { $0.isEmpty ? nil : $0 } ?? "/bin/bash"
+
+    let handler: (() -> Void) = {
+      let process = Process()
+      process.executableURL = URL(fileURLWithPath: shell)
+      process.arguments = ["-c", command]
+      try? process.run()
+    }
+
+    return handler
+  }
+
   public let identifier = UUID()
 
   public var keyCode: UInt32?
