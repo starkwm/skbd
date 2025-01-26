be6858935ac91e32c47b733122a05f8d9b451b3d
Tom Bell <tomb@tombell.dev>
2025-01-26 22:53:44 +0000

    Add handler(for:) static method to Shortcut for creating a handler function

--- Tests/skbdlibTests/ShortcutTests.swift
+++ Tests/skbdlibTests/ShortcutTests.swift
@@ -1,6 +1,65 @@
+import Carbon
 import Testing
 
 @testable import skbdlib
+
+@Suite("Shortcut", .serialized)
+struct ShortcutSerializedTests {
+  // MARK: Shortcut.handler(for:)
+
+  @Test("Shortcut.handler(for:) (with SHELL set)")
+  func handlerWithShell() async throws {
+    setenv("SHELL", "/bin/bash", 1)
+
+    #expect(throws: Never.self) {
+      let handler = Shortcut.handler(for: "true")
+
+      try handler()
+    }
+  }
+
+  @Test("Shortcut.handler(for:) (with empty SHELL set)")
+  func handlerWithEmptyShell() async throws {
+    setenv("SHELL", "", 1)
+
+    #expect(throws: Never.self) {
+      let handler = Shortcut.handler(for: "true")
+
+      try handler()
+    }
+  }
+
+  @Test("Shortcut.handler(for:) (with unset SHELL set)")
+  func handlerWithUnsetShell() async throws {
+    unsetenv("SHELL")
+
+    #expect(throws: Never.self) {
+      let handler = Shortcut.handler(for: "true")
+
+      try handler()
+    }
+  }
+
+  @Test("Shortcut.handler(for:) (with invalid SHELL set)")
+  func handlerWithInvalidShell() async throws {
+    setenv("SHELL", "/bin/invalid", 1)
+
+    #expect(throws: Error.self) {
+      let handler = Shortcut.handler(for: "true")
+
+      try handler()
+    }
+  }
+
+  @Test("Shortcut.handler(for:) (with empty command)")
+  func handlerWithEmptyCommand() async throws {
+    #expect(throws: Never.self) {
+      let handler = Shortcut.handler(for: "")
+
+      try handler()
+    }
+  }
+}
 
 @Suite("Shortcut")
 struct ShortcutTests {
