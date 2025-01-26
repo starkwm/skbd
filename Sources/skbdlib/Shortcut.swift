189011e374183b2923ea45597d2fb2d4bfab8679
Tom Bell <tomb@tombell.dev>
2025-01-26 22:53:44 +0000

    Add handler(for:) static method to Shortcut for creating a handler function

--- Sources/skbdlib/Shortcut.swift
+++ Sources/skbdlib/Shortcut.swift
@@ -1,13 +1,29 @@
 import Carbon
 import Foundation
 
+public typealias CommandHandler = () throws -> Void
+
 public struct Shortcut {
+  public static func handler(for command: String) -> CommandHandler {
+    let shell = ProcessInfo.processInfo.environment["SHELL"].flatMap { $0.isEmpty ? nil : $0 } ?? "/bin/bash"
+
+    let handler: CommandHandler = {
+      if command.isEmpty {
+        return
+      }
+
+      try Process.run(URL(fileURLWithPath: shell), arguments: ["-c", command])
+    }
+
+    return handler
+  }
+
   public let identifier = UUID()
 
   public var keyCode: UInt32?
   public var modifierFlags: UInt32?
 
-  public var handler: (() -> Void)!
+  public var handler: CommandHandler!
 
   public init() {}
 
@@ -16,7 +32,7 @@ public struct Shortcut {
     self.modifierFlags = modifierFlags
   }
 
-  public init(_ keyCode: UInt32, _ modifierFlags: UInt32, _ handler: @escaping (() -> Void)) {
+  public init(_ keyCode: UInt32, _ modifierFlags: UInt32, _ handler: @escaping CommandHandler) {
     self.keyCode = keyCode
     self.modifierFlags = modifierFlags
     self.handler = handler
