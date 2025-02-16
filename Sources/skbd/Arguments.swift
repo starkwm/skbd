f5aaac3a0d7b230a7c4c16de0c4593057d27c85b
Tom Bell <tomb@tombell.dev>
2025-02-16 20:31:35 +0000

    Update skbd to use ConfigManager

--- Sources/skbd/Arguments.swift
+++ Sources/skbd/Arguments.swift
@@ -2,12 +2,12 @@ import ArgumentParser
 import Foundation
 
 struct Arguments: ParsableArguments {
-  @Option(name: .shortAndLong, help: ArgumentHelp("Path to the configuration file", valueName: "path"))
-  var config: String = FileManager
-    .default
-    .homeDirectoryForCurrentUser
-    .appending(path: ".config/skbd/skbdrc")
-    .path()
+  @Option(
+    name: .shortAndLong,
+    help: ArgumentHelp("Path to a configuration file, or directory of configuration files", valueName: "path"),
+    transform: URL.init(fileURLWithPath:)
+  )
+  var config: URL = FileManager.default.homeDirectoryForCurrentUser.appending(path: ".config/skbd")
 
   @Flag(name: .shortAndLong, help: "Reload the configuration file")
   var reload = false
