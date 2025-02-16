6d0cf925a27e624df0d314dd631225687a412066
Tom Bell <tomb@tombell.dev>
2025-02-16 20:31:20 +0000

    Add ConfigManager class

---
+++ Sources/skbdlib/ConfigManager.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+public class ConfigManager {
+  private var shortcutManager: ShortcutManager
+
+  private var configPath: URL
+
+  public init(configPath: URL) {
+    self.shortcutManager = ShortcutManager()
+    self.configPath = configPath
+  }
+
+  deinit {
+    self.shortcutManager.reset()
+    self.shortcutManager.stop()
+  }
+
+  public func load() throws {
+    shortcutManager.reset()
+
+    try parseConfig(filePath: configPath)
+  }
+
+  public func start() -> Bool {
+    return shortcutManager.start()
+  }
+
+  public func stop() {
+    shortcutManager.stop()
+  }
+
+  private func parseConfig(filePath: URL) throws {
+    var isDirectory: ObjCBool = false
+
+    if !FileManager.default.fileExists(atPath: filePath.path, isDirectory: &isDirectory) {
+      throw ConfigError.configurationDoesNotExist
+    }
+
+    if isDirectory.boolValue {
+      try parseConfigDir(filePath: filePath)
+    } else {
+      try parseConfigFile(filePath: filePath)
+    }
+  }
+
+  private func parseConfigDir(filePath: URL) throws {
+    let files = try FileManager.default.contentsOfDirectory(
+      at: filePath,
+      includingPropertiesForKeys: nil,
+      options: .skipsHiddenFiles
+    )
+
+    for file in files {
+      try parseConfigFile(filePath: file)
+    }
+  }
+
+  private func parseConfigFile(filePath: URL) throws {
+    let config = try String(contentsOf: filePath)
+    let parser = Parser(config)
+    let shortcuts = try parser.parse()
+
+    for shortcut in shortcuts {
+      shortcutManager.register(shortcut: shortcut)
+    }
+  }
+}
