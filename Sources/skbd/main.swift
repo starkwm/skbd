50e186909e2ab0ee7bc1d3ca302da50b987f1e8b
Tom Bell <tomb@tombell.dev>
2025-02-16 20:31:35 +0000

    Update skbd to use ConfigManager

--- Sources/skbd/main.swift
+++ Sources/skbd/main.swift
@@ -1,7 +1,7 @@
 import AppKit
 import skbdlib
 
-let shortcutManager = ShortcutManager()
+var config: ConfigManager!
 
 func main() -> Int32 {
   if arguments.version {
@@ -16,7 +16,7 @@ func main() -> Int32 {
       kill(pid, SIGUSR1)
       return EXIT_SUCCESS
     } catch {
-      fputs("error reading pid from lockfile: \(error)\n", stderr)
+      fputs("failed to read pid from lock file: \(error)\n", stderr)
       return EXIT_FAILURE
     }
   }
@@ -24,52 +24,40 @@ func main() -> Int32 {
   do {
     try LockFile.acquire()
   } catch {
-    fputs("error creating lockfile file: \(error)\n", stderr)
+    fputs("failed to create lock file: \(error)\n", stderr)
     return EXIT_FAILURE
   }
 
   do {
-    let config = try String(contentsOfFile: arguments.config)
-    let parser = Parser(config)
-    let shortcuts = try parser.parse()
+    config = ConfigManager(configPath: arguments.config)
 
-    for shortcut in shortcuts {
-      shortcutManager.register(shortcut: shortcut)
-    }
+    try config.load()
 
-    if !shortcutManager.start() {
-      fputs("error starting the shortcut manager", stderr)
+    if !config.start() {
+      fputs("failed to start configuration manager", stderr)
       return EXIT_FAILURE
     }
   } catch {
-    fputs("error parsing configuration file: \(error)\n", stderr)
+    fputs("failed to read configuration: \(error)\n", stderr)
     return EXIT_FAILURE
   }
 
   signal(SIGUSR1) { _ in
     do {
-
       fputs("received SIGUSR1 - reloading configuration...\n", stdout)
       fflush(stdout)
 
-      let config = try String(contentsOfFile: arguments.config)
-      let parser = Parser(config)
-      let shortcuts = try parser.parse()
-
-      shortcutManager.reset()
-
-      for shortcut in shortcuts {
-        shortcutManager.register(shortcut: shortcut)
-      }
+      try config.load()
     } catch {
-      fputs("error parsing configuration file: \(error)\n", stderr)
+      fputs("failed to reload configuration: \(error)\n", stderr)
     }
   }
 
   signal(SIGINT) { _ in
     fputs("received SIGINT - terminating...\n", stdout)
     fflush(stdout)
-    shortcutManager.stop()
+
+    config = nil
     exit(EXIT_SUCCESS)
   }
 
