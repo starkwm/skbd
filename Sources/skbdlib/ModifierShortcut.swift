e837ad499c160c5ec30ed44a6b620d026a44611f
Tom Bell <tomb@tombell.dev>
2025-02-26 20:52:50 +0000

    Remove empty init() for ModifierShortcut

--- Sources/skbdlib/ModifierShortcut.swift
+++ Sources/skbdlib/ModifierShortcut.swift
@@ -11,8 +11,6 @@ struct ModifierShortcut: Shortcut {
   var keyCode: UInt32?
   var modifierFlags: UInt32?
 
-  init() {}
-
   init(_ keyCode: UInt32, _ modifierFlags: UInt32) {
     self.keyCode = keyCode
     self.modifierFlags = modifierFlags
