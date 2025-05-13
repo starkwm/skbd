096fc9c9d093fc6d37b7179dc2a2ae265dd13def
Tom Bell <tomb@tombell.dev>
2025-05-13 23:20:48 +0100

    wip: ui

---
+++ Sources/skbdlib/UI/PanelWindow.swift
@@ -0,0 +1,15 @@
+import AppKit
+
+public class PanelWindow: NSPanel {
+  init(contentRect: NSRect) {
+    super.init(
+      contentRect: contentRect,
+      styleMask: [.nonactivatingPanel],
+      backing: .buffered,
+      defer: false
+    )
+
+    isFloatingPanel = true
+    isOpaque = false
+  }
+}
